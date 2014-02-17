8890df25acbe25c3a49370dd5921a71a49e02210
Vignal-Fanny <vignalfa@PC-B101-01.iut-valence.fr>
2014-02-17 10:35:25 +0100

    Déplacement du projet dans un package
    On a déplacé MasterMind.java dans fr.iutvalence.java.tp.mastermind.

---
+++ src/fr/iutvalence/java/tp/mastermind/MasterMind.java
@@ -0,0 +1,16 @@
+package fr.iutvalence.java.tp.mastermind;
+
+/**
+ *  Représente une partie de MasterMind
+ * @author vignalfa
+ *
+ */
+public class MasterMind
+{
+	/**
+	 * Pour vérifier si la partie est en cours
+	 */
+	private static boolean partieEstFinie = false;
+			
+	
+}
